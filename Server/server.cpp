5541c40212c20b13696e308f7e7f7a4be5b8d61e
Romeu Pires <rilpires@gmail.com>
2018-10-17 14:00:14 -0300

    network fixing

--- Server/server.cpp
+++ Server/server.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 SpaceServer::SpaceServer(std::string ip_address ){
+    using namespace std;
     if ( enet_initialize() ){
 		//std::cout << "Couldn't initialize enet" << std::endl;
     }
@@ -23,7 +24,10 @@ SpaceServer::SpaceServer(std::string ip_address ){
         while( enet_host_service(host , &event , 1000.0 / 60.0 ) > 0 ){
             switch( event.type ){
                 case ENET_EVENT_TYPE_CONNECT:
-                    std::cout << "someone joined:" << event.peer->address.host << std::endl;
+                    char ip_address[256];
+                    enet_address_get_host_ip( &(event.peer->address) , ip_address , 256 );
+                    cout << "someone joined:" << std::endl;
+                    cout << "host_ip: " << ip_address << endl;
                     addNewPeer( event.peer );
                     break;
                 case ENET_EVENT_TYPE_DISCONNECT:
@@ -44,7 +48,13 @@ SpaceServer::SpaceServer(std::string ip_address ){
 void SpaceServer::addNewPeer( ENetPeer* peer ){
     peers.push_back( peer );
 
-    ENetPacket* packet = enet_packet_create( (void*)&(peer->address) , sizeof(ENetAddress) , ENET_PACKET_FLAG_RELIABLE );
+    ENetAddress address;
+    address.port = peer->address.port;
+    char ip_address[256];
+    enet_address_get_host_ip( &(peer->address) , ip_address , 256 );
+    enet_address_set_host( &address , ip_address );
+    
+    ENetPacket* packet = enet_packet_create( (void*)&address , sizeof(ENetAddress) , ENET_PACKET_FLAG_RELIABLE );
     for( auto it = peers.begin() ; it != peers.end() ; it ++) if( *it != peer ){
         enet_peer_send( *it , 0 , packet );
     }
